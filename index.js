210dab405d83baf7a516b2d9484d9b03142332f6
Guto Xavier <gutoffline@gmail.com>
2026-03-18 22:25:38 -0300

    modulo de usuarios

--- index.js
+++ index.js
@@ -70,4 +70,33 @@ app.post("/produto/", function (req, res) {
   });
 })
 
+
+
+let usuarios = ["paulo", "cris"]
+
+app.get("/usuarios/", function (req, res) {
+  res.send(usuarios)
+})
+
+app.post("/usuarios/", function (req, res) {
+  const item = req.body.nome
+  usuarios.push(item)
+  res.send("Item adicionado com sucesso!")
+})
+
+app.put("/usuarios/:id", function (req, res) {
+  const id = req.params.id - 1
+  const novoItem = req.body.nome
+  usuarios[id] = novoItem
+  res.send("Item atualizado com sucesso!")
+})
+
+app.delete("/usuarios/:id", function (req, res) {
+  const id = req.params.id - 1
+  delete usuarios[id]
+  res.send("Item removido com sucesso!")
+}) 
+
+
+
 app.listen(3000)
